74b014d9861333840ed3e28a302443c1d15dbbad
original <git@github.com:denis-poroshin/variables.git>
2024-07-11 20:23:35 +0300

    Начало

---
+++ src/main/java/ru/bank/unknown/UnknownApplication.java
@@ -0,0 +1,13 @@
+package ru.bank.unknown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnknownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnknownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bank/unknown/UnknownApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bank.unknown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnknownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
